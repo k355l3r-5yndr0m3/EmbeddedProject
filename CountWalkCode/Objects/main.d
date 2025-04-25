1b3f31194b5b41c396dea731d9d85690f586f7c3
k355l3r-5yndr0m3 <hoanghung17jan@gmail.com>
2025-04-25 15:03:07 +0700

    Change

--- CountWalkCode/Objects/main.d
+++
@@ -1,8 +0,0 @@
-./objects/main.o: main.c RTE\_Target_1\RTE_Components.h \
-  RTE\Device\STM32F103C8\RTE_Device.h \
-  C:\users\cha0s\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
-  C:\users\cha0s\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm3.h \
-  C:\users\cha0s\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h \
-  C:\users\cha0s\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\RTE_Driver\GPIO_STM32F10x.h \
-  C:\users\cha0s\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Driver\Include\Driver_I2C.h \
-  C:\users\cha0s\AppData\Local\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Driver\Include\Driver_Common.h
